4ace9da59feb712e226ddce1992be2eae004d28a
Wolfgang Fahl <wf@bitplan.com>
2012-06-26 07:27:42 +0200

    adds TestSuite

---
+++ test/com/bitplan/storage/sql/TestSuite.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) 2012 BITPlan GmbH
+ *
+ * Pater-Delp-Str. 1
+ * D-47877 Willich-Schiefbahn
+ *
+ * http://www.bitplan.com
+ * 
+ */
+
+package com.bitplan.storage.sql;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ TestSqlStorage.class})
+/**
+ * TestSuite
+ * @author wf
+ *
+ * no content necessary - annotation has info
+ */
+public class TestSuite {
+}
