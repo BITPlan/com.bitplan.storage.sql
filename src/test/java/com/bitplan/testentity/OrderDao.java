38f9241c6d810dcbedad23f052c8e783cf5c79b7
Wolfgang Fahl <wf@bitplan.com>
2012-11-24 17:51:02 +0100

    adds dao interfaces

---
+++ src/test/java/com/bitplan/testentity/OrderDao.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C) 2011-2012 BITPlan GmbH
+ * 
+ * Pater-Delp-Str. 1
+ * D-47877 Willich-Schiefbahn
+ *
+ * http://www.bitplan.com
+ * 
+ * generated: 2012-11-24 09:15 with smartGENERATOR
+ */
+package com.bitplan.testentity;
+import com.bitplan.testentity.Order;
+import com.bitplan.testentity.OrderDao;
+import com.bitplan.testentity.Customer;
+import com.bitplan.testentity.CustomerDao;
+
+/**
+ * interface for Order
+ */
+public interface OrderDao {
+	// >>>{interface}{Order}{Order}
+	// no implementation yet !!!
+	// <<<{interface}{Order}{Order}
+  /**
+   * getter for id
+   * @return id
+   */
+  public long getId();
+
+  /**
+   * setter for id
+   * @param pid - new value for id
+   */
+  public void setId(long pid);
+
+  /**
+   * getter for shipping address
+   * @return address
+   */
+  public String getAddress();
+
+  /**
+   * setter for shipping address
+   * @param paddress - new value for shipping address
+   */
+  public void setAddress(String paddress);
+
+  /**
+   * getter for customer
+   * @return customer
+   */
+  public Customer getCustomer();
+
+  /**
+   * setter for customer
+   * @param pcustomer - new value for customer
+   */
+  public void setCustomer(Customer pcustomer);
+
+} // OrderDao
